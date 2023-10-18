3264816685534312a058c203b04041b934cba024
Fedasevich <yurafedasevka@gmail.com>
2023-10-18 20:57:02 +0200

    * fix imports

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { Module, forwardRef } from "@nestjs/common";
-import { JWTGuardRegisterModule } from "src/guards/jwt-auth/jwt-guard-register.module";
+import { JWTGuardRegisterModule } from "../guards/jwt-auth/jwt-guard-register.module";
 import { UsersModule } from "../users/users.module";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
