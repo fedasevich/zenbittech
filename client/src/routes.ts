f76a4ba946a791b56415c405facd910fb9bfcaee
Fedasevich <yurafedasevka@gmail.com>
2023-10-18 15:18:56 +0200

    * auth routes

--- client/src/routes.ts
+++ client/src/routes.ts
@@ -1,4 +1,4 @@
-import { AUTH_ROUTE, MAIN_ROUTE } from './libs/constants/routes';
+import { MAIN_ROUTE, SIGN_IN_ROUTE, SIGN_UP_ROUTE } from './libs/constants/routes';
 import { AuthPage } from './pages/AuthPage';
 import { MainPage } from './pages/MainPage';
 
@@ -8,7 +8,11 @@ export const publicRoutes = [
     Component: MainPage
   },
   {
-    path: AUTH_ROUTE,
+    path: SIGN_IN_ROUTE,
+    Component: AuthPage
+  },
+  {
+    path: SIGN_UP_ROUTE,
     Component: AuthPage
   }
 ];
