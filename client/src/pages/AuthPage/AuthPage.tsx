4c64883ff9879e963a9a433950a290cefa1ed7ad
Fedasevich <yurafedasevka@gmail.com>
2023-10-18 19:47:17 +0200

    * folder

--- client/src/pages/AuthPage/AuthPage.tsx
+++ client/src/pages/AuthPage/AuthPage.tsx
@@ -3,16 +3,20 @@ import { StyledAuthFormImage } from '#/components/AuthForm/AuthFormImage';
 import { Flex } from '#/components/Flex/Flex';
 import styled from 'styled-components';
 
-const StyledAuthPage = styled(Flex)`
+export const StyledAuthPage = styled(Flex)`
   height: calc(100vh - 80px);
   width: 100%;
 `;
 
-export function AuthPage() {
+interface AuthPageProps {
+  children?: React.ReactNode;
+}
+
+export function AuthPage({ children }: AuthPageProps) {
   return (
     <StyledAuthPage>
       <StyledAuthFormImage />
-      <AuthForm />
+      {children || <AuthForm />}
     </StyledAuthPage>
   );
 }
