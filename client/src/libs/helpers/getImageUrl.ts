e7b8b8cb551d2d3b6c82b897d8b20addbeaefb3e
Fedasevich <yurafedasevka@gmail.com>
2023-10-18 15:19:36 +0200

    * image url helper

---
+++ client/src/libs/helpers/getImageUrl.ts
@@ -0,0 +1,6 @@
+import { IMAGES } from '../constants/images';
+import { ValueOf } from '../types/ValueOf/valueOf.type';
+
+export const getImageUrl = (url: ValueOf<typeof IMAGES> | string) => {
+  return `${import.meta.env.VITE_API_URL}/${url}`;
+};
