73e5291bca9fd80a233607e475e40ad516340ed9
Fedasevich <yurafedasevka@gmail.com>
2023-10-18 15:22:48 +0200

    * auth form

--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -1,3 +1,18 @@
+import { AuthForm } from '#/components/AuthForm/AuthForm';
+import { StyledAuthFormImage } from '#/components/AuthForm/AuthFormImage';
+import { Flex } from '#/components/Flex/Flex';
+import styled from 'styled-components';
+
+const StyledAuthPage = styled(Flex)`
+  height: calc(100vh - 80px);
+  width: 100%;
+`;
+
 export function AuthPage() {
-  return <></>;
+  return (
+    <StyledAuthPage>
+      <StyledAuthFormImage />
+      <AuthForm />
+    </StyledAuthPage>
+  );
 }
