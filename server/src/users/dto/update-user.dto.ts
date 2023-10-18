6d0c53a6808c8f63e58d8817646600d0bad14386
Fedasevich <yurafedasevka@gmail.com>
2023-10-18 19:50:15 +0200

    * update user dto

--- server/src/users/dto/update-user.dto.ts
+++ server/src/users/dto/update-user.dto.ts
@@ -1,21 +1,7 @@
-import { ApiPropertyOptional } from "@nestjs/swagger";
-import { IsEmail, IsOptional, IsString, Length } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
 export class UpdateUserDto {
-  @ApiPropertyOptional({
-    example: "test@gmail.com",
-    description: "Email of the user",
-  })
-  @IsOptional()
-  @IsString({ message: "Must be string" })
-  @IsEmail({}, { message: "Wrong e-mail" })
-  email: string;
-
-  @ApiPropertyOptional({
-    example: "12345",
-    description: "Password of the user",
-  })
-  @IsOptional()
-  @IsString({ message: "Must be string" })
-  @Length(4, 16, { message: "From 4 to 16 symbols" })
+  @ApiProperty({ example: "1234", description: "New user password." })
+  @IsString()
   password: string;
 }
