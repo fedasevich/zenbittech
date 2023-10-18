c401a9fc139ff61c68156be0ab26822988a75214
Fedasevich <yurafedasevka@gmail.com>
2023-10-18 19:46:39 +0200

    * reset password routes

--- client/src/routes.ts
+++ client/src/routes.ts
@@ -1,11 +1,20 @@
-import { MAIN_ROUTE, SIGN_IN_ROUTE, SIGN_UP_ROUTE } from './libs/constants/routes';
-import { AuthPage } from './pages/AuthPage';
+import {
+  MAIN_ROUTE,
+  RESET_PASSWORD_CONFIRM_ROUTE,
+  RESET_PASSWORD_ROUTE,
+  SIGN_IN_ROUTE,
+  SIGN_UP_ROUTE
+} from './libs/constants/routes';
+import { AuthPage } from './pages/AuthPage/AuthPage';
+import { ResetPasswordConfirmPage } from './pages/AuthPage/ResetPasswordConfirmPage';
+import { ResetPasswordPage } from './pages/AuthPage/ResetPasswordPage';
 import { MainPage } from './pages/MainPage';
 
 export const publicRoutes = [
   {
     path: MAIN_ROUTE,
-    Component: MainPage
+    Component: MainPage,
+    isNavBarShow: true
   },
   {
     path: SIGN_IN_ROUTE,
@@ -14,5 +23,13 @@ export const publicRoutes = [
   {
     path: SIGN_UP_ROUTE,
     Component: AuthPage
+  },
+  {
+    path: RESET_PASSWORD_ROUTE,
+    Component: ResetPasswordPage
+  },
+  {
+    path: RESET_PASSWORD_CONFIRM_ROUTE,
+    Component: ResetPasswordConfirmPage
   }
 ];
