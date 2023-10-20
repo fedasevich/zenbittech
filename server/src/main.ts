a82f5b360a49a029f295cddc28933a3286116c78
Fedasevich <yurafedasevka@gmail.com>
2023-10-20 23:00:58 +0200

    * swagger cdn

--- server/src/main.ts
+++ server/src/main.ts
@@ -15,7 +15,15 @@ async function start() {
     .addTag("fullstack")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
+
+  SwaggerModule.setup("/api", app, document, {
+    customCssUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+    customJs: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js",
+    ],
+  });
 
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
