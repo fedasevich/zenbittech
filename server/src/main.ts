e85ac22dc3bf30f211e5598db6625005e4a61391
Fedasevich <yurafedasevka@gmail.com>
2023-10-20 22:39:27 +0200

    * swagger docs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,4 +1,5 @@
 import { HttpAdapterHost, NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { AllExceptionsFilter } from "./filters/all-exceptions.filter";
 import { ValidationPipe } from "./pipes/validation.pipe";
@@ -6,6 +7,15 @@ import { ValidationPipe } from "./pipes/validation.pipe";
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Zenbittech")
+    .setDescription("The test case")
+    .setVersion("1.0")
+    .addTag("fullstack")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
 
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
